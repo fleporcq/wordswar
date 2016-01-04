fe76b5e79b69af1161c41e6ec03b55ef29a5c7b6
François LEPORCQ <fleporcq@gmail.com>
2016-01-04 10:59:46 +0100

    feat(database): mise en place de doctrine

--- index.php
+++ index.php
@@ -1,12 +1,42 @@
 <?php
 
-use WordsWar\Model\Language;
 use WordsWar\GameFactory;
+use WordsWar\Model\Language;
 
 require_once('vendor' . DIRECTORY_SEPARATOR . 'autoload.php');
 
 $gameFactory = GameFactory::getInstance();
+/* @var $game \WordsWar\Model\Game */
 $game = $gameFactory->createNewGame(Language::FR(), 6, 6);
-$dictionary = $game->getDictionary();
-var_dump($dictionary->wordExists('truculentes'));
+$game->start();
 $grid = $game->getGrid();
+
+
+$config = new \Doctrine\DBAL\Configuration();
+// the connection configuration
+$connectionParams = [
+    'url'   => 'sqlite:///:memory:',
+];
+$conn = \Doctrine\DBAL\DriverManager::getConnection($connectionParams, $config);
+
+?>
+
+<table border="1" width="400">
+<?php
+$width = $grid->getWidth();
+/* @var $cell \WordsWar\Model\Cell */
+foreach($grid->getCells() as $key => $cell):
+    $key++;
+    $tile = $cell->getTile();
+    ?>
+    <?php if($key == 1 || $key%$width == 1):?>
+        <tr>
+    <?php endif;?>
+
+            <td><?php echo strtoupper($tile); ?><small><?php echo $tile->getScore(); ?></small></td>
+        
+   <?php if($key%$width == 0):?>
+        </tr>
+    <?php endif;?>
+<?php endforeach; ?>
+</table>
